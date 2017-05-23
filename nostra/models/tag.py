62507686e98df82d3d868b8bb2cb1e9dc0332cfb
James Baker <jamesbaker@Jamess-MacBook-Pro.local>
2017-05-22 22:50:21 -0400

    Initialize Nostra

---
+++ nostra/models/tag.py
@@ -0,0 +1,17 @@
+from nostra.extensions import db
+from nostra.models.relationships import tags_posts
+from nostra.database import (
+    Model,
+    SurrogatePK,
+)
+
+
+class Tag(SurrogatePK, Model):
+
+    __tablename__ = 'tags'
+
+    tag = db.Column(db.Text)
+    posts = db.relationship('Post', secondary=tags_posts, backref=db.backref('tags_br', lazy='dynamic'))
+
+    def __init__(self, tag, **kwargs):
+        db.Model.__init__(self, tag=tag, **kwargs)
